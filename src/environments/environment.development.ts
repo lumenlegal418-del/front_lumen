840566099c1a8153d38ab33c906ae0e48720d4aa
David <david.lopez.rivera.2003@gmail.com>
2026-09-13 12:45:12 -0500

    change api route

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://backend-lu.onrender.com/api/v1',
+  apiUrl: 'https://api.lumenfinance.com.co/api/v1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Cuando despliegues el FastAPI, reemplaza esto por la URL pública (ej: https://api.lumenlegal.com/api/v1)
-  apiUrl: 'https://backend-lu.onrender.com/api/v1',
+  apiUrl: 'https://api.lumenfinance.com.co/api/v1',
 };
